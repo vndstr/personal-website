96397498542e220237bfa52d3aaa96fcfae74343
Alvin Adisatria <54824753+vndstr@users.noreply.github.com>
2020-09-14 14:33:20 +0700

    Add files via upload

---
+++ assets/script/app.js
@@ -0,0 +1,15 @@
+function sendEmail() {
+    window.location.href = "mailto:designwithalvin@gmail.com";
+}
+
+function dribbbleLink() {
+    window.location.href = "https://dribbble.com/uixalvin"
+}
+
+function submitDTC() {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScx_exYqyGMNfhVn8wOLp8UH-LDN3s8uYoJ8DUT8fpTl_PHLQ/viewform"
+}
+
+function toBottom() {
+    window.location.href = "#skills"
+}
